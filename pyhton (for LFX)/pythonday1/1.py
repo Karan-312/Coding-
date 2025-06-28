9f5bd4fd2f5f85a18b4b75d28662d4f8ac726964
karan312 <rudrawalramesh29@gmail.com>
2025-06-29 03:06:40 +0530

    just a test change

--- pyhton (for LFX)/pythonday1/1.py
+++ pyhton (for LFX)/pythonday1/1.py
@@ -3,3 +3,10 @@ print("hello, LFX world")
 name = input("enter you name : ")
 age = input("enter your age :")
 print(" hello " + name + " you are  " + age + " years old . ")
+
+print("hello`, LFX world")
+print("hello, LFX world")       
+
+
+print("hello, LFX world")
+print("hello, LFX world")
