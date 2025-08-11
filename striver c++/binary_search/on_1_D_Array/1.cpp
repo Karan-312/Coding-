ddfd402ec5051dbcd3cfac20a8555e4f9bb1de66
karan <rudrawalramesh29@gmail.com>
2025-08-11 17:31:30 +0530

    dsa practice update

--- striver c++/binary_search/on_1_D_Array/1.cpp
+++ striver c++/binary_search/on_1_D_Array/1.cpp
@@ -28,27 +28,28 @@
 
 //recursive method for binary search 
 
-#include<bits/stdc++.h>
-using namespace std ;
+// #include<bits/stdc++.h>
+// using namespace std ;
 
-int binary_search (vector<int> & arr , int low , int high , int target){
+// int binary_search (vector<int> & arr , int low , int high , int target){
 
-    while (low > high)
-    {
-        return -1;
-    }
-    int mid = ((low + high)/2);
+//     while (low > high)
+//     {
+//         return -1;
+//     }
+//     int mid = ((low + high)/2);
 
-    if ( target == arr[mid]) return mid;
-    else if ( target > mid ) {
-        binary_search(arr , mid +1 , high , target);
-    }
-    else {
-        binary_search(arr , low , mid - 1 , target);
-    }
+//     if ( target == arr[mid]) return mid;
+//     else if ( target > mid ) {
+//         binary_search(arr , mid +1 , high , target);
+//     }
+//     else {
+//         binary_search(arr , low , mid - 1 , target);
+//     }
     
-}
-int main () {
-    vector<int> karan = {12,17,199,204,309};
-    cout << binary_search(karan,0,karan.size()-1,1000);
-}
+// }
+// int main () {
+//     vector<int> karan = {12,17,199,204,309};
+//     cout << binary_search(karan,0,karan.size()-1,1000);
+// }
+
--- striver c++/binary_search/on_1_D_Array/practice.cpp
+++ striver c++/binary_search/on_1_D_Array/practice.cpp
@@ -1,24 +1,113 @@
+// #include<bits/stdc++.h>
+// using namespace std ;
+
+// int search(vector<int> arr , int n , int target ){
+    
+//     int low = 0 ;
+//     int high = n - 1 ;
+    
+
+//     // while( low < high ){
+
+//     //         int mid  =  ( high + low ) / 2 ;
+
+//     //         if ( target < mid ) {
+
+//     //             high = mid ;
+
+//     //         }
+//     //         else if ( target > mid ) {
+
+//     //             low  = mid ; 
+
+//     //         }
+//     //         else if  ( target == mid ) { 
+
+//     //             return mid;
+
+//     //         }
+//     // }
+
+//     // return -1;
+
+// }
+
+
+// int main () {
+//     vector<int> arr = {3,4,6,7,9,12,16,17};
+//     int n = arr.size();
+//     int target = 6 ;
+//     cout << search(arr , n , target);
+//     return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std ;
+
+// int search ( vector<int> & arr , int target , int low , int high){
+
+//     if ( low > high ) return -1;
+//     int mid = ( low + high ) / 2 ;
+//     if ( target == arr[mid] ) return arr[mid];
+//     else if ( target > arr[mid]) return search( arr , target , mid +1 , high);
+//     else if ( target < arr[mid]) return search( arr , target , low , mid-1);
+    
+// }
+
+// int binary_lengthing(vector<int> & arr , int n , int target ) {
+//     return search(arr , target , 0, arr.size() - 1 );
+// }
+
+// int main () {
+//     vector<int> arr = {3,4,6,7,9,12,16,17};
+//     int n = arr.size();
+//     int target = 6 ;
+//     cout << binary_lengthing(arr , n , target);
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std ;
 
-int binary_search ( vector<int> & arr , int low , int high , int target){
+int finding_smallest_index(vector<int> & arr , int n , int x){
+//     // int index_count = 0 ;
+// //     for( int i = 0 ; i < n ; i++ ){
+// //         if ( arr[i] >= x ){
+// //             return i;
+// //             // break;
+// //         }
+// //     }
+// //     return -1 ;
+ //}
+        int low = 0;
+        int high = n-1;
+ 
+    if ( low > high) return -1 ;
+       
+    int mid =( low + high ) / 2 ;  
 
-while ( low > high)
-{
+    if ( x == arr[mid] ) return mid ;
 
-    return -1 ;
-}
+    else if ( x > arr[mid] ) {
+    for ( int i = mid +1  ; i < n ; i++) {
+        if (arr[i] >= x ){
+            return i ;
+            }
+        }
+    }
 
-int mid = ( low + high )/2;
-
-if ( target == mid ){ return mid };
-else if ( target > mid ) binary_function( arr , mid + 1 , high, target );
-else binary_function(arr,low,mid-1,target);
-
-return -1;
+    else if ( x < arr[mid] ){
+        for (int i = mid  ; i > 0 ; i = i - 1 ) {
+            if ( arr[i] >= x ) {
+                return i ;
+            }
+        }
+    } 
 }
 
 int main () {
-    vector<int> arr = [12,]
-    cout << binary_function()
+    vector<int> arr = {1,2,3,3,6,8,9,9,9,11};
+    int n = arr.size();
+    cout << finding_smallest_index(arr , n , 4); 
+    return 0 ;
 }
